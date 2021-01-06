40f67ed666e7c536439f4fcac049d49099b9875a
lucas-miller-at-382183931171 <lucas-miller-at-382183931171>
2021-01-05 22:30:04 -0300

    Subindo swagger

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require('./config/swagger.json');
 
-const port = 8005;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.get("/customers/:email", (req, res) => {
+  res.status(200).send("...");
 });
 
-app.get("/", (req, res) => {
-    let date = new Date();
-    res.status(200);
-    return res.send(`OK ${date.toISOString()}`);
-})
+app.post("/customers", (req, res) => {
+    res.status(200).send("...");
+});
+  
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
